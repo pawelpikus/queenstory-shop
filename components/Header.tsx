4cfdc6f28d2ad86be1ced4d244f07733452d83d1
godspeed7 <pawelpikus7@gmail.com>
2022-02-22 17:32:34 +0100

    Adding nav to header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className=" bg-slate-700 py-8 ">
-      <div className="w-4/5 mx-auto text-white">Navigation</div>
+      <nav className="w-4/5 mx-auto text-white">Navigation</nav>
     </header>
   );
 };
--- pages/users.tsx
+++
@@ -1,5 +0,0 @@
-const Home = () => {
-  return <div>users</div>;
-};
-
-export default Home;
