a2f85a9776336e549ac8d88f737f5cf06b51cf34
Pawel Pikus <pawelpikus7@gmail.com>
2022-03-19 16:32:33 +0100

    Adding Default Seo to app

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,35 +7,27 @@ import { routes } from "../routes/routes";
 
 const Home = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-neutral-50">
-      <Header />
-      <main className="flex-grow ">
-        <div className="w-11/12 mx-auto mt-8 md:mt-32 lg:mt-34 max-w-7xl text-neutral-800">
-          <div className="flex items-center w-full gap-8">
-            <div className="flex flex-col items-start justify-center w-full gap-6">
-              <h1 className="mb-4 text-4xl font-extrabold md:text-6xl lg:text-7xl">
-                Discover the beauty within.
-              </h1>
-              <Link href={`${routes.PRODUCTS}/1`}>
-                <a className="px-12 py-4 font-bold text-white transition-colors border-none rounded shadow-lg bg-neutral-800 hover:bg-gradient-to-tl from-emerald-500 to-emerald-800 ">
-                  Explore
-                </a>
-              </Link>
-            </div>
-            <div className="flex flex-col items-center justify-center w-full ml-8">
-              <div className="rounded-lg shadow-lg bg-neutral-800 w-fit">
-                <Image
-                  src={logo}
-                  alt="Queen Story logo"
-                  className="max-w-full rounded-lg"
-                  priority
-                />
-              </div>
-            </div>
-          </div>
+    <div className="flex items-center w-full gap-8">
+      <div className="flex flex-col items-start justify-center w-full gap-6">
+        <h1 className="mb-4 text-4xl font-extrabold md:text-6xl lg:text-7xl">
+          Discover the beauty within.
+        </h1>
+        <Link href={`${routes.PRODUCTS}/1`}>
+          <a className="px-12 py-4 font-bold text-white transition-colors border-none rounded shadow-lg bg-neutral-800 hover:bg-gradient-to-tl from-emerald-500 to-emerald-800 ">
+            Explore
+          </a>
+        </Link>
+      </div>
+      <div className="flex flex-col items-center justify-center w-full ml-8">
+        <div className="rounded-lg shadow-lg bg-neutral-800 w-fit">
+          <Image
+            src={logo}
+            alt="Queen Story logo"
+            className="max-w-full rounded-lg"
+            priority
+          />
         </div>
-      </main>
-      <Footer />
+      </div>
     </div>
   );
 };
--- pages/products/item/[productId].tsx
+++ pages/products/item/[productId].tsx
@@ -3,7 +3,6 @@ import ErrorPage from "next/error";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import React from "react";
-import Layout from "../../../components/Layout";
 import { ProductDetails } from "../../../components/Product";
 import { ProductSkeleton } from "../../../components/ProductSkeleton";
 
